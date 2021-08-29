572469358b856f4b8b163292e95144e74fe6b77f
unknown <md.rafiu.islam@g.bracu.ac.bd>
2021-08-30 03:20:59 +0600

    basic header stucture formed with search bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,41 @@
+import { useState } from "react";
+import { Toolbar, Typography, AppBar, Box, InputBase } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+// import { Autocomplete } from "@react-google-maps/api";
+
 import React from "react";
+import useStyles from "./styles";
 
 const Header = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <AppBar position="static">
+            <Toolbar className={classes.toolbar}>
+                <Typography variant="h5" className={classes.title}>
+                    Trippy
+                </Typography>
+                <Box display="flex">
+                    <Typography variant="h6" className={classes.title}>
+                        Explore the world!
+                    </Typography>
+                    {/* <Autocomplete> */}
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon />
+                        </div>
+                        <InputBase
+                            placeholder="Search ..."
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput,
+                            }}
+                        />
+                    </div>
+                    {/* </Autocomplete> */}
+                </Box>
+            </Toolbar>
+        </AppBar>
     );
 };
 
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import googleMapReact from "google-map-react";
+import { Paper, Typography } from "@material-ui/core";
+
+import useStyles from "./styles";
 
 const Map = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery("(min-width:600px)");
+
     return (
         <div>
             <h1>Map</h1>
